7da4adbf80e98917cd81a8a046cdac9969feb525
nilsoncthomas <nilsoncthomas@gmail.com>
2023-03-27 16:33:27 -1200

    commit

---
+++ bin/datatypes.dart
@@ -0,0 +1,17 @@
+void main (){
+  /// variable declaration
+  /// datatype variable_name = value;
+ String name = "nilson";
+ int age = 27;
+ int phonenumber = 9633621402;
+ double mark = 20;
+ String email = "nilsoncthomas@gmail.com";
+
+ print("student detailes");
+ print("my name is $name");//String interpolation
+ print("i am $age yrs old");
+ print("my phone number is $phonenumber");
+ print("cgpa in graduation is $mark");
+ print("my mail id is $email");
+
+}
