8f15789072c33843909a52193c11caaaa77cef3c
nilsoncthomas <nilsoncthomas@gmail.com>
2023-03-29 05:33:32 -1200

    commit

---
+++ bin/varriabledeclaration.dart
@@ -0,0 +1,10 @@
+void main(){
+  String x = "hello";
+  //type allocated according to the initial value =>var
+  var y = 20;
+  //y = "hello"; not possible
+  //y = 40;
+  // type change according to value change => dynamic
+  dynamic z = "hello";
+          z = 30;
+}
